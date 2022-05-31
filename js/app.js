7d0a88268b7fc040fd3699286cccad678a6e27b8
JakeD <jakediefenderfer@gmail.com>
2022-05-31 06:25:41 -0400

    code cleanup

--- js/app.js
+++ js/app.js
@@ -16,9 +16,8 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
       do {
-        getGuess()
+        prevGuesses.push(getGuess())
         render()
-        prevGuesses.push(guess)
       } while (guess !== this.secretNum)
   },
   getGuess: function() {
@@ -32,65 +31,8 @@ const game = {
   }
 }
 
+// game.getGuess()
+// game.play()
 
 
-// while (sku !== 'quit') {
-//   sku = prompt('Enter SKU or "quit" to exit: ')
-//   if (sku in catalog) {
-//     let price = catalog[sku]
-//     alert(`The price of ${sku} is ${price}`) 
-//   } else if (sku !== 'quit') {
-//     alert('Invalid SKU - try again')
-//   }
-
-
-
-
-
-
-
-// console.log(game)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const catalog = {
-//   AB4747: 4.50,
-//   CD5876: 10.25,
-//   DG9856: 15.75
-// }
-
-// let sku = ''
-
-// while (sku !== 'quit') {
-//   sku = prompt('Enter SKU or "quit" to exit: ')
-//   if (sku in catalog) {
-//     let price = catalog[sku]
-//     alert(`The price of ${sku} is ${price}`) 
-//   } else if (sku !== 'quit') {
-//     alert('Invalid SKU - try again')
-//   }
-    
-
-//   }
-
-//   for (let key in game) {
-//     console.log(`The value of the ${key} property is ${game[key]}`)
-//   }
-
-// game.play = function() {
-//   this.secretNum = Math.floor(Math.random() * 
-//     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-// }
+// felt like I had something going then got lost in my own work. Can't figure out why this won't work.
